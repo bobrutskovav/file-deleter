f63526e09a722df0c6322536eb91ff1cdc2f223a
bobrutskovav <thirdstrike@yandex.ru>
2017-05-04 23:12:28 +0300

    add Cli Class,dont works rightnow

--- src/main/java/CliHander.java
+++ src/main/java/CliHander.java
@@ -1,5 +1,34 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
 /**
  * Created by aleksx on 04.05.2017.
  */
 public class CliHander {
+
+    private Options options;
+    private CommandLine line;
+
+    public CliHander() {
+
+        options = new Options();
+        setOptions();
+    }
+
+
+    private void setOptions() {
+        options.addOption("h", "Print help");
+        options.addOption("s", "service", true, "Starts program like a service, set's cooldown period exmpl: 30m or 1h");
+        options.addOption("fe", "file extension", true, "File extension to delete ,exmpl: .exe or .part1.rar, the default value is .torrent");
+    }
+
+    public void parse(String[] args) throws Exception {
+
+        DefaultParser parser = new DefaultParser();
+        line = parser.parse(options, args);
+        if (line.hasOption("help")) {
+            throw new Exception("Print help and exit");
+        }
+    }
 }

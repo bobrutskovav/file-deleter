0d69aba8dbe3ade8a5c87544a898e2c20bd26905
ABobrutskov <avbobrutskov.sbt@sberbank.ru>
2017-05-23 12:06:04 +0300

    weakRef added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 /**
@@ -7,35 +8,35 @@ class Main {
     public static void main(String[] args) {
         Application app;
         TrayController controller;
-        CliHandler cliHandler = new CliHandler();
+        WeakReference<CliHandler> cliHandler = new WeakReference<>(new CliHandler());
 
         try {
-            cliHandler.parse(args);
-            ArrayList<String> fileExtensions = cliHandler.getFileExtensions();
+            cliHandler.get().parse(args);
+            ArrayList<String> fileExtensions = cliHandler.get().getFileExtensions();
             if (fileExtensions.isEmpty()) {
                 fileExtensions.add(".torrent");
             }
-            ArrayList<String> ingoredExtensions = cliHandler.getIgnoredFileExtensions();
+            ArrayList<String> ingoredExtensions = cliHandler.get().getIgnoredFileExtensions();
             app = new Application(fileExtensions, ingoredExtensions);
 
-            if (cliHandler.isService()) {
+            if (cliHandler.get().isService()) {
                 app.setService(true);
-                String timeToRestart = cliHandler.getCooldownTime();
+                String timeToRestart = cliHandler.get().getCooldownTime();
                 app.setTimer(new Timer(timeToRestart));
                 controller = new TrayController(app);
                 controller.makeATray();
 
             }
-            String isOlderValue = cliHandler.getOlderThenPerion();
+            String isOlderValue = cliHandler.get().getOlderThenPerion();
             app.setPeriodToDelete(isOlderValue);
-            app.setDeepSearch(cliHandler.getDeepSearch());
-            app.setNeedDeleteToBin(cliHandler.getIsNotToBin());
+            app.setDeepSearch(cliHandler.get().getDeepSearch());
+            app.setNeedDeleteToBin(cliHandler.get().getIsNotToBin());
 
             app.start();
             System.exit(0);
 
         } catch (Exception e) {
-            cliHandler.printCliHelp();
+            cliHandler.get().printCliHelp();
             e.printStackTrace();
             System.exit(0);
         }
